6b3b2f41722bb298427de7166d7e251aa2a03b3b
yuxiulong <642400877@qq.com>
2021-01-18 15:20:27 +0800

    解决参数与跨域问题

---
+++ src/main/java/com/boraydata/hygiene/web/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.boraydata.hygiene.web.config;
+
+import com.boraydata.hygiene.web.Interceptor.ProcessInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //注册TestInterceptor拦截器
+        InterceptorRegistration registration = registry.addInterceptor(new ProcessInterceptor());
+        registration.addPathPatterns("/**");                      //所有路径都被拦截
+        registration.excludePathPatterns(                         //添加不拦截路径
+        );
+    }
+
+}
--- src/main/java/com/boraydata/hygiene/web/controller/IndexController.java
+++ src/main/java/com/boraydata/hygiene/web/controller/IndexController.java
@@ -5,12 +5,14 @@ import com.boraydata.hygiene.web.business.PopulationBusiness;
 import com.boraydata.hygiene.web.request.EconomicsRequest;
 import com.boraydata.hygiene.web.request.PopulationRequest;
 import com.boraydata.hygiene.web.result.MyResult;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 @RequestMapping("/main")
 public class IndexController {
 
@@ -22,6 +24,7 @@ public class IndexController {
 
     @RequestMapping("/find/population")
     public MyResult findPopulation(@RequestBody PopulationRequest populationRequest) {
+        log.info("123123");
         return MyResult.success(populationBusiness.listPopulationInfo(populationRequest));
     }
 
